7c5f8f54d4f56d0123d74b96d971e1e1e47d16f9
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-02-24 12:53:15 +1300

    Add pink class and three function

--- classes.js
+++ classes.js
@@ -7,6 +7,7 @@ function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
   one()
   two()
+  three()
 
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -26,5 +27,9 @@ function two () {
 }
 
 // CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('pink')
+}
 
 // CREATE FUNCTION makeVisible HERE
