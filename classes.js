497ffe6816a7d9b3d9288924a79088100ea58067
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-02-24 15:32:27 +1300

    Make invisible visible

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
   one()
   two()
   three()
+  makeVisible()
 
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -33,3 +34,7 @@ function three () {
 }
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+  makeVisible.classList.add('visible')
+}
